5bc368a12fbb00e603f50c4d3ec2aff06daab30d
Diak Maksym <flaxyakk@gmail.com>
2024-03-08 11:45:25 +0200

    Initial commit

---
+++ Lab 7.2/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7._2
+{
+    internal class Program
+    {
+    
+    interface ICommand
+    {
+        void Execute();
+        void Undo();
+    }
+
+    
+    class AddCommand : ICommand
+    {
+        private readonly Calculator _calculator;
+        private readonly double _value;
+
+        public AddCommand(Calculator calculator, double value)
+        {
+            _calculator = calculator;
+            _value = value;
+        }
+
+        public void Execute()
+        {
+            _calculator.Add(_value);
+        }
+
+        public void Undo()
+        {
+            _calculator.Subtract(_value);
+        }
+    }
+
+    
+    class SubtractCommand : ICommand
+    {
+        private readonly Calculator _calculator;
+        private readonly double _value;
+
+        public SubtractCommand(Calculator calculator, double value)
+        {
+            _calculator = calculator;
+            _value = value;
+        }
+
+        public void Execute()
+        {
+            _calculator.Subtract(_value);
+        }
+
+        public void Undo()
+        {
+            _calculator.Add(_value);
+        }
+    }
+
+        // Команда множення
+        class MultiplyCommand : ICommand
+        {
+            private readonly Calculator _calculator;
+            private readonly double _value;
+
+            public MultiplyCommand(Calculator calculator, double value)
+            {
+                _calculator = calculator;
+                _value = value;
+            }
+
+            public void Execute()
+            {
+                _calculator.Multiply(_value);
+            }
+
+            public void Undo()
+            {
+                
+                _calculator.Divide(_value);
+            }
+        }
+
+       
+        class DivideCommand : ICommand
+        {
+            private readonly Calculator _calculator;
+            private readonly double _value;
+
+            public DivideCommand(Calculator calculator, double value)
+            {
+                _calculator = calculator;
+                _value = value;
+            }
+
+            public void Execute()
+            {
+                _calculator.Divide(_value);
+            }
+
+            public void Undo()
+            {
+                
+                _calculator.Multiply(_value);
+            }
+        }
+
+        
+        class Calculator
+        {
+            private double _result;
+
+            public void Add(double value)
+            {
+                _result += value;
+            }
+
+            public void Subtract(double value)
+            {
+                _result -= value;
+            }
+
+            public void Multiply(double value)
+            {
+                _result *= value;
+            }
+
+            public void Divide(double value)
+            {
+                if (value != 0)
+                {
+                    _result /= value;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero.");
+                }
+            }
+
+            public double GetResult()
+            {
+                return _result;
+            }
+        }
+
+        
+        class CommandExecutor
+    {
+        private readonly Stack<ICommand> _commands = new Stack<ICommand>();
+
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+            _commands.Push(command);
+        }
+
+        public void Undo()
+        {
+            if (_commands.Count > 0)
+            {
+                var lastCommand = _commands.Pop();
+                lastCommand.Undo();
+            }
+        }
+    }
+
+    static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            var executor = new CommandExecutor();
+
+            executor.Execute(new AddCommand(calculator, 10));
+            executor.Execute(new SubtractCommand(calculator, 5));
+            executor.Execute(new MultiplyCommand(calculator, 3));
+            executor.Execute(new DivideCommand(calculator, 2));
+            executor.Undo();
+
+            Console.WriteLine($"Результат: {calculator.GetResult()}"); 
+        }
+    }
+}
---
+++ Lab 7/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Order
+    {
+        public string Customer { get; set; }
+        public int OrderSize { get; set; }
+        public string ShippingMethod { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+
+    abstract class OrderHandler
+    {
+        protected OrderHandler successor;
+
+        public void SetSuccessor(OrderHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleOrder(Order order);
+    }
+
+    class SizeHandler : OrderHandler
+    {
+        public override void HandleOrder(Order order)
+        {
+            if (order.OrderSize <= 10)
+            {
+                Console.WriteLine($"Order for {order.Customer} is a small order.");
+            }
+            else if (order.OrderSize <= 30)
+            {
+                Console.WriteLine($"Order for {order.Customer} is an average-sized order.");
+            }
+            else
+            {
+                Console.WriteLine($"Order for {order.Customer} is a large order.");
+            }
+
+            if (successor != null)
+            {
+                successor.HandleOrder(order);
+            }
+        }
+    }
+
+    class ShippingHandler : OrderHandler
+    {
+        public override void HandleOrder(Order order)
+        {
+            if (order.ShippingMethod == "Courier")
+            {
+                Console.WriteLine($"Order for {order.Customer} will be delivered by courier.");
+            }
+            else if (order.ShippingMethod == "Pickup")
+            {
+                Console.WriteLine($"Order for {order.Customer} will be available for pickup.");
+            }
+
+            if (successor != null)
+            {
+                successor.HandleOrder(order);
+            }
+        }
+    }
+
+    class PaymentHandler : OrderHandler
+    {
+        public override void HandleOrder(Order order)
+        {
+            if (order.PaymentMethod == "Card")
+            {
+                Console.WriteLine($"Order for {order.Customer} will be paid by card.");
+            }
+            else if (order.PaymentMethod == "Cash")
+            {
+                Console.WriteLine($"Order for {order.Customer} will be paid by cash.");
+            }
+            else if (order.PaymentMethod == "Online")
+            {
+                Console.WriteLine($"Order for {order.Customer} will be paid online.");
+            }
+
+            if (successor != null)
+            {
+                successor.HandleOrder(order);
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderHandler sizeHandler = new SizeHandler();
+            OrderHandler shippingHandler = new ShippingHandler();
+            OrderHandler paymentHandler = new PaymentHandler();
+
+            sizeHandler.SetSuccessor(shippingHandler);
+            shippingHandler.SetSuccessor(paymentHandler);
+
+            Order testOrder = new Order
+            {
+                Customer = "Max",
+                OrderSize = 15,
+                ShippingMethod = "Courier",
+                PaymentMethod = "Online"
+            };
+
+            sizeHandler.HandleOrder(testOrder);
+        }
+    }
+}
